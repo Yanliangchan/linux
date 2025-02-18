a789f55b3b9f1e227986726a1dee8aa23b2a3a62
Yanliangchan <118790760+Yanliangchan@users.noreply.github.com>
2025-02-18 16:11:15 +0800

    [Init] Modified kernel_init() to launch a single application at boot

--- init/main.c
+++ init/main.c
@@ -103,6 +103,7 @@
 #include <linux/pidfs.h>
 #include <linux/ptdump.h>
 #include <net/net_namespace.h>
+#include <linux/sched/signal.h>
 
 #include <asm/io.h>
 #include <asm/setup.h>
@@ -1447,77 +1448,14 @@ void __weak free_initmem(void)
 
 static int __ref kernel_init(void *unused)
 {
-	int ret;
+    printk(KERN_INFO "Running single application at boot...\n");
 
-	/*
-	 * Wait until kthreadd is all set-up.
-	 */
-	wait_for_completion(&kthreadd_done);
+    char *argv[] = { "/bin/app", NULL };
+    char *envp[] = { "HOME=/", NULL };
 
-	kernel_init_freeable();
-	/* need to finish all async __init code before freeing the memory */
-	async_synchronize_full();
+    call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
 
-	system_state = SYSTEM_FREEING_INITMEM;
-	kprobe_free_init_mem();
-	ftrace_free_init_mem();
-	kgdb_free_init_mem();
-	exit_boot_config();
-	free_initmem();
-	mark_readonly();
-
-	/*
-	 * Kernel mappings are now finalized - update the userspace page-table
-	 * to finalize PTI.
-	 */
-	pti_finalize();
-
-	system_state = SYSTEM_RUNNING;
-	numa_default_policy();
-
-	rcu_end_inkernel_boot();
-
-	do_sysctl_args();
-
-	if (ramdisk_execute_command) {
-		ret = run_init_process(ramdisk_execute_command);
-		if (!ret)
-			return 0;
-		pr_err("Failed to execute %s (error %d)\n",
-		       ramdisk_execute_command, ret);
-	}
-
-	/*
-	 * We try each of these until one succeeds.
-	 *
-	 * The Bourne shell can be used instead of init if we are
-	 * trying to recover a really broken machine.
-	 */
-	if (execute_command) {
-		ret = run_init_process(execute_command);
-		if (!ret)
-			return 0;
-		panic("Requested init %s failed (error %d).",
-		      execute_command, ret);
-	}
-
-	if (CONFIG_DEFAULT_INIT[0] != '\0') {
-		ret = run_init_process(CONFIG_DEFAULT_INIT);
-		if (ret)
-			pr_err("Default init %s failed (error %d)\n",
-			       CONFIG_DEFAULT_INIT, ret);
-		else
-			return 0;
-	}
-
-	if (!try_to_run_init_process("/sbin/init") ||
-	    !try_to_run_init_process("/etc/init") ||
-	    !try_to_run_init_process("/bin/init") ||
-	    !try_to_run_init_process("/bin/sh"))
-		return 0;
-
-	panic("No working init found.  Try passing init= option to kernel. "
-	      "See Linux Documentation/admin-guide/init.rst for guidance.");
+    while (1);  // Prevent any other process from running
 }
 
 /* Open /dev/console, for stdin/stdout/stderr, this should never fail */
